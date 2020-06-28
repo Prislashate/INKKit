0030f8363f80af1bad61efdc926fa8a896e956e1
pirate <treess@aliyun.com>
2020-06-28 15:28:15 +0800

    Update

---
+++ Sources/tool/main.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Numerics
+
+print("########################################")
+print("CommandLine arguments（\(CommandLine.arguments.count)）: \(CommandLine.arguments)")
+print("########################################")
+
+
+
+
